d320a596a2f670e0ed0f4a866886f319b9e0cbdf
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-11-20 21:31:54 +0000

    Fixed the data that appeared in each profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,8 +3,12 @@
 @section('content')
 <div class="container">
     @auth
-    <h3>{{ Auth::user()->name }}</h3>
-    <p>@<span>{{ Auth::user()->username }}</span></p>
+    <h3>{{ $user->name}}</h3>
+    <p>@<span>{{ $user->username }}</span></p>
     @endauth
+
+    @if(Auth::id() === $user->id)
+        <p><em>This is your profile</em></p>
+    @endif
 </div>
 @endsection
