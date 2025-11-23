4539db60f561915cca8dbcf6b31aa9dc6453f134
João Pedro Pinto Lunet <up202207150@edu.fe.up.pt>
2025-11-23 16:19:54 +0000

    integrated book api

--- config/services.php
+++ config/services.php
@@ -38,7 +38,11 @@ return [
     'spotify' => [
         'client_id' => env('SPOTIFY_CLIENT_ID'),
         'client_secret' => env('SPOTIFY_CLIENT_SECRET'),
-        'redirect' => env('SPOTIFY_REDIRECT_URI'), 
+        'redirect' => env('SPOTIFY_REDIRECT_URI'),
+    ],
+
+    'google_books' => [
+        'api_key' => env('GOOGLE_BOOKS_API_KEY'),
     ],
 
 ];
