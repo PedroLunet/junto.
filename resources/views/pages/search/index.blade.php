2972a2b49a4327f9eb8606402e231fae63ae0c5c
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2025-11-27 17:27:04 +0000

    Fixed styling of user cards in search users

--- resources/views/pages/search/index.blade.php
+++ resources/views/pages/search/index.blade.php
@@ -1,32 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<main id="search-users-page">
-    <h1>Search for a user</h1>
-    <p>Start by typing your friend's name!</p>
+    <main class="w-full max-w-4xl mx-auto flex flex-col items-center gap-8 px-4 py-8">
+        <h1 class="text-4xl font-black text-center">Search for a user</h1>
+        <p class="-mt-12 text-center">Start by typing your friend's name!</p>
 
-    <form method="get">
-        <input value="{{ old('query', request('query')) }}" type="text" name="query" />
-        <x-button>Search</x-button>
-    </form>
+        <form method="get" class="flex flex-row gap-4 items-start w-full">
+            <input value="{{ old('query', request('query')) }}" type="text" name="query" class="max-w-3/4 w-full" />
+            <x-button>Search</x-button>
+        </form>
 
-    <div id="user-list">
-        @forelse ($users as $user)
-        @unless ($user->id == Auth::user()->id)
-        <x-user-card
-            :user="$user"
-            :showUnfriendButton="in_array($user->id, $friends)"
-            :showBefriendButton="!in_array($user->id, $friends)"
-            :friendButtonData="!in_array($user->id, $friends) ? $friendService->getFriendButtonData($user)  : null"
-            :unfriendRoute="route('friends.unfriend', $user->id)"
-            :confirmMessage="'Are you sure you want to unfriend ' . $user->name . '?'" />
-
-        @endunless
-        @empty
-        <div class="col-span-full text-left text-gray-500 text-sm">
-            <p id="no-results">No results match your filters, try adjusting them.</p>
+        <div class="w-full flex flex-col gap-4">
+            @forelse ($users as $user)
+                @unless ($user->id == Auth::user()->id)
+                    <x-user-card :user="$user" :showUnfriendButton="in_array($user->id, $friends)" :showBefriendButton="!in_array($user->id, $friends)" :friendButtonData="!in_array($user->id, $friends) ? $friendService->getFriendButtonData($user) : null" :unfriendRoute="route('friends.unfriend', $user->id)"
+                        :confirmMessage="'Are you sure you want to unfriend ' . $user->name . '?'" />
+                @endunless
+            @empty
+                <div class="col-span-full text-left text-gray-500 text-sm">
+                    <p>No results match your filters, try adjusting them.</p>
+                </div>
+            @endforelse
         </div>
-        @endforelse
-    </div>
-</main>
+    </main>
 @endsection
