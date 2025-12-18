c6135d9439dba465fec9e058d0793d6aace6acb1
Maria Luiza Henriques Vieira <up202304306@edu.fe.up.pt>
2025-12-18 11:38:27 +0000

    added loading for sending emails

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -17,7 +17,7 @@
                     </div>
                 @endif
 
-                <form method="POST" action="{{ route('contact.submit') }}">
+                <form id="contact-form" method="POST" action="{{ route('contact.submit') }}">
                     @csrf
 
                     <x-ui.input 
@@ -51,7 +51,7 @@
                     />
 
                     <div class="flex justify-center">
-                        <x-ui.button type="submit" variant="primary" class="w-full md:w-auto px-8 py-3 text-lg">
+                        <x-ui.button id="submit-button" type="submit" variant="primary" class="w-full md:w-auto px-8 py-3 text-lg">
                             Send Message
                         </x-ui.button>
                     </div>
@@ -59,4 +59,15 @@
             </div>
         </div>
     </div>
+
+    <script>
+        document.getElementById('contact-form').addEventListener('submit', function(e) {
+            const button = document.getElementById('submit-button');
+            
+            // Disable button and show loading state
+            button.disabled = true;
+            button.innerHTML = '<i class="fas fa-spinner fa-spin mr-2"></i> Sending...';
+            button.classList.add('opacity-75', 'cursor-not-allowed');
+        });
+    </script>
 @endsection
