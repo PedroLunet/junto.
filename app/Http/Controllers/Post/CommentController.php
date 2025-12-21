e3c7e0f83e32722ee857d5a208ba324911376e62
João Pedro Pinto Lunet <up202207150@edu.fe.up.pt>
2025-12-21 17:24:27 +0000

    added new function for editing comment o n controller

--- app/Http/Controllers/Post/CommentController.php
+++ app/Http/Controllers/Post/CommentController.php
@@ -30,4 +30,21 @@ class CommentController extends Controller
         'comment' => $comment
     ], 201); 
 }
+
+    public function update(Request $request, $commentId)
+    {
+        $comment = Comment::findOrFail($commentId);
+        $this->authorize('update', $comment);
+
+        $request->validate([
+            'content' => 'required|string|max:1000'
+        ]);
+
+        Comment::updateComment($commentId, $request->input('content'));
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Comment updated successfully!'
+        ]);
+    }
 }
